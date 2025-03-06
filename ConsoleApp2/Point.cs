32f47784ad499fe02bb3c732d80f4dc4705aa257
Ku4erenko15 <159886914+Ku4erenko15@users.noreply.github.com>
2025-03-06 11:08:15 +0200

    Add files via upload

---
+++ ConsoleApp2/Point.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Point
+    {
+        private double x;
+        private double y;
+
+        public double X
+            {
+            get { return x; }
+            set { x = value; }
+            }
+        public double Y
+        {
+            get { return y; }
+            set {  y = value; }
+        }
+
+        public Point() {}
+        public Point(double x, double y)
+        {
+            this.X = x;
+                this.Y = y;
+        }
+
+        //public static void Write(Point p1, Point p2, Point p3, Point p4)
+        //{
+        //    Console.WriteLine($"Point 1: ({p1.X}, {p1.Y}), Point 2: ({p2.X}, {p2.Y}, Point 3({p3.X},{p3.Y}), Point 4: ({p4.X},{p4.Y}))");
+        //}
+
+        public override string ToString()
+        {
+            return $"X={X}\tY={Y}";
+        }
+
+
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Point[] points = new Point[4]
+            //{
+            //    new Point(2,3),
+            //    new Point(-3,4),
+            //    new Point(-2,-5),
+            //    new Point (2,-5)
+            //};
+
+            Point[] points = new Point[4];
+            for (int i = 0; i < points.Length; i++)
+            {
+                Console.WriteLine("Введите координаты {0}-й точки", i + 1);
+                double x = Convert.ToDouble(Console.ReadLine());
+                double y = Convert.ToDouble(Console.ReadLine());
+                points[i] = new Point(x,y);
+            }
+
+
+
+
+
+
+                for (int i = 0; i < points.Length; i++)
+            {
+                Console.WriteLine(points[i].ToString());
+
+            }
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
